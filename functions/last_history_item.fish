2713904e07159da7f67c22e262ee54fc4b84b63b
Aiden Langley <aiden@nedia.dev>
2026-09-22 16:42:11 +1200

    feat: !! now adds sudo, like zsh

--- functions/last_history_item.fish
+++ functions/last_history_item.fish
@@ -1,4 +1,4 @@
-function last_history_item --description 'Repeat last command, combines with \
-  abbr to place last command in prompt'
-    echo $history[1]
+function last_history_item --description 'Repeat last command with sudo, \
+  combines with abbr to place last command in prompt'
+    echo "sudo $history[1]"
 end
