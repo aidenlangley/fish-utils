0591ba8c4d8cdcf547bb818629e20875e64d4ef1
Aiden Langley <aiden@nedia.dev>
2026-09-22 16:43:20 +1200

    fix: include completions for sunriseset

---
+++ completions/sunriseset.fish
@@ -0,0 +1,3 @@
+complete -c format --short h --long help --description 'Print help and exit'
+complete -c format --short t --long latitude --description 'Set latitude'
+complete -c format --short g --long longitude --description 'Set longitude'
